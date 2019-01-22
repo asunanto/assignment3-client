6e040415452cd5db1602cc97c981661d07d7fc7b
Shadow-Blitz <skyedexin@gmail.com>
2019-01-22 14:29:35 +1000

    Cleaning code in Index.js

--- src/index.js
+++ src/index.js
@@ -4,31 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './config/store';
-import NavBar from './components/HeaderComponent/NavBar.js';
-import {
-  Route,
-  Switch,
-  BrowserRouter as Router
-} from 'react-router-dom'
 
 const render = () => ReactDOM.render(<App />, 
   document.getElementById('root'));
-
-const routing = (
-  <Router>
-       <div>
-           <NavBar />
-           <Switch>
-               <Route path="/" component={App} />
-               {/* <Route path="/users" component={SignUp} />
-               <Route exact path="/contact" component={Login} /> */}
-               {/* <Route component={Notfound} /> */}
-           </Switch>
-       </div>
-   </Router>
-)
-
-ReactDOM.render(routing, document.getElementById('root'))
 
 store.subscribe(render)
 render()
