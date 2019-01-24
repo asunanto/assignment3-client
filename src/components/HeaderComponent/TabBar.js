678f40a5e62e6735b538f73abf0948bd48ca6f87
Shadow-Blitz <skyedexin@gmail.com>
2019-01-24 15:51:03 +1000

    Working on TabBar responsiveness

--- src/components/HeaderComponent/TabBar.js
+++ src/components/HeaderComponent/TabBar.js
@@ -7,18 +7,11 @@ class TabBar extends Component {
     return (
       <footer>
         <TabBarStyles>
-        <Link to="/"><i style={{color:(window.location.pathname==='/' ? '#EC963A':'#F5F9FF')}}className="material-icons">account_circle</i><div class="row">
-    <div class="col-xs-12
-                col-sm-8
-                col-md-6
-                col-lg-4">
-        <div class="box">Responsive</div>
-    </div>
-</div></Link>
-        <Link to="/"><i style={{color:(window.location.pathname==='/' ? '#EC963A':'#F5F9FF')}}className="material-icons">home</i></Link>
-        <Link to="/signup"><i style={{color:(window.location.pathname==='/signup' ? '#EC963A':'#F5F9FF')}}className="material-icons">description</i></Link>
-        <Link to="/login"><i style={{color:(window.location.pathname==='/login' ? '#EC963A':'#F5F9FF')}}className="material-icons">list</i></Link>
-        <Link to="/login"><i style={{color:(window.location.pathname==='/login' ? '#EC963A':'#F5F9FF')}}className="material-icons">info</i></Link>
+        <Link to="/user"><i style={{color:(window.location.pathname==='/user' ? '#EC963A':'#F5F9FF'), 'fontSize': '10vw'}}className="material-icons">account_circle</i></Link>
+        <Link to="/unit"><i style={{color:(window.location.pathname==='/unit' ? '#EC963A':'#F5F9FF'), 'fontSize': '10vw'}}className="material-icons">home</i></Link>
+        <Link to="/program"><i style={{color:(window.location.pathname==='/program' ? '#EC963A':'#F5F9FF'), 'fontSize': '10vw'}}className="material-icons">description</i></Link>
+        <Link to="/activities"><i style={{color:(window.location.pathname==='/activities' ? '#EC963A':'#F5F9FF'), 'fontSize': '10vw'}}className="material-icons">list</i></Link>
+        <Link to="/login"><i style={{color:(window.location.pathname==='/login' ? '#EC963A':'#F5F9FF'), 'fontSize': '10vw'}}className="material-icons">info</i></Link>
         </TabBarStyles>
       </footer>
     )
--- src/styles/TabBarStyles.js
+++ src/styles/TabBarStyles.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 const TabBarStyles = styled.div`
-position: absolute;
-font-size: 40px;
+position: fixed;
 bottom: 0;
-background: #0033A1;
 left: 0;
 right: 0;
+background: #0033A1;
+
 `
 export default TabBarStyles
