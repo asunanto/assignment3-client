a1e3d0a94171d4c7554856a4db92910a471979a3
Shadow-Blitz <skyedexin@gmail.com>
2019-01-24 13:20:05 +1000

    Created TabBar, still needs work

--- src/components/HeaderComponent/TabBar.js
+++ src/components/HeaderComponent/TabBar.js
@@ -7,11 +7,12 @@ class TabBar extends Component {
     return (
       <footer>
         <TabBarStyles>
-        <Link to="/">Home</Link>
-        { '  |  ' }
-        <Link to="/signup">Sign Up</Link>
-        { '  |  ' }
-        <Link to="/login">Login</Link>
+        
+        <Link to="/"><i style={{color:(window.location.pathname==='/' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">account_circle</i></Link>
+        <Link to="/"><i style={{color:(window.location.pathname==='/' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">home</i></Link>
+        <Link to="/signup"><i style={{color:(window.location.pathname==='/signup' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">description</i></Link>
+        <Link to="/login"><i style={{color:(window.location.pathname==='/login' ? '#EC963A':'#F5F9FF'), 'marginRight': '20px'}}className="material-icons">list</i></Link>
+        <Link to="/login"><i style={{color:(window.location.pathname==='/login' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">info</i></Link>
         </TabBarStyles>
       </footer>
     )
--- src/styles/TabBarStyles.js
+++ src/styles/TabBarStyles.js
@@ -2,9 +2,11 @@ import styled from 'styled-components'
 
 const TabBarStyles = styled.div`
 position: absolute;
+font-size: 40px;
 bottom: 0;
-background: #005dab;
+background: #0033A1;
 left: 0;
 right: 0;
+if 
 `
 export default TabBarStyles
