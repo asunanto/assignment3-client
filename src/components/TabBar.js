7bd6b86307ff5f97942c4203881c141324a9a437
Danylo Denysov <danyldezwaan@hotmail.com>
2019-01-25 10:22:55 +1000

    updating the TapBar

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -7,12 +7,12 @@ class TabBar extends Component {
     return (
       <footer>
         <TabBarStyles>
-        
-        <Link to="/user"><i style={{color:(window.location.pathname==='/user' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">account_circle</i></Link>
-        <Link to="/unit"><i style={{color:(window.location.pathname==='/unit' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">home</i></Link>
-        <Link to="/create-program"><i style={{color:(window.location.pathname==='/create-program' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">description</i></Link>
-        <Link to="/library"><i style={{color:(window.location.pathname==='/library' ? '#EC963A':'#F5F9FF'), 'marginRight': '20px'}}className="material-icons">list</i></Link>
-        <Link to="/about"><i style={{color:(window.location.pathname==='/about' ? '#EC963A':'#F5F9FF'),'marginLeft': '20px', 'marginRight': '20px'}}className="material-icons">info</i></Link>
+
+          <Link to="/user"><i style={{ color: (window.location.pathname === '/user' ? '#EC963A' : '#F5F9FF'), 'flex': '1', 'fontSize': '5vw' }} className="material-icons">account_circle</i></Link>
+          <Link to="/unit"><i style={{ color: (window.location.pathname === '/unit' ? '#EC963A' : '#F5F9FF'), 'flex': '1', 'fontSize': '5vw' }} className="material-icons">home</i></Link>
+          <Link to="/create-program"><i style={{ color: (window.location.pathname === '/create-program' ? '#EC963A' : '#F5F9FF'), 'flex': '1', 'fontSize': '5vw' }} className="material-icons">description</i></Link>
+          <Link to="/library"><i style={{ color: (window.location.pathname === '/library' ? '#EC963A' : '#F5F9FF'), 'flex': '1', 'fontSize': '5vw' }} className="material-icons">list</i></Link>
+          <Link to="/about"><i style={{ color: (window.location.pathname === '/about' ? '#EC963A' : '#F5F9FF'), 'flex': '1', 'fontSize': '5vw' }} className="material-icons">info</i></Link>
         </TabBarStyles>
       </footer>
     )
