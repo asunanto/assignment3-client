727aa2bbd38f08d4af131063ed7747234633e942
watsbeat <c.watsonbeaton@gmail.com>
2019-02-05 19:46:19 +1000

    updated for pull request

--- src/components/CreateActivity.js
+++ src/components/CreateActivity.js
@@ -29,26 +29,25 @@ class CreateActivity extends Component {
     const token = localStorage.getItem("token")
     setJwt(token)
     e.preventDefault()
-    try {
-      const { title, description, len } = e.target.elements
-      console.log('title, description, length', title.value, description.value, len.value)
-      const length= len.value
-      if (!length.match(/[a-z][A-Z]/g) || length<1 || length >120) {
-        alert ('Length must be between 1 and 120 minutes and digits only')
-        return
-      }
+    // try {
+    //   const { title, description, len } = e.target.elements
+    //   console.log('title, description, length', title.value, description.value, len.value)
+    //   const length= len.value
+    //   if (!length.match(/[a-z][A-Z]/g) || length<1 || length >120) {
+    //     alert ('Length must be between 1 and 120 (minutes). Enter digits only, please.')
+    //   }
       
-      const req = {
-        title: title.value,
-        description: description.value,
-        length: len.value,
-        ageLevel: this.state.ageLevel
-      }
-      addActivity(req)
-      console.log(req.length)
-      this.props.history.push(`/user`)
-    }
-    catch (error) { console.error(error) }
+    //   const req = {
+    //     title: title.value,
+    //     description: description.value,
+    //     length: len.value,
+    //     ageLevel: this.state.ageLevel
+    //   }
+    //   addActivity(req)
+    //   console.log(req.length)
+    //   this.props.history.push(`/user`)
+    // }
+    // catch (error) { console.error(error) }
   }
 
   render() {
@@ -111,4 +110,5 @@ class CreateActivity extends Component {
     )
   }
 }
+
 export default CreateActivity;
