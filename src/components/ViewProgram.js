667cc1cfa44394c0e70e75863e30e7d485b83a62
watsbeat <c.watsonbeaton@gmail.com>
2019-02-07 16:16:47 +1000

    display activities belonging to a program in a grid list

--- src/components/ViewProgram.js
+++ src/components/ViewProgram.js
@@ -1,9 +1,11 @@
 // View a Single Program with its Activities (Grid List)
 
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core/';
+import { Button, Grid, Link, Fab } from '@material-ui/core/';
+import AddIcon from '@material-ui/icons/Add';
 import store from '../config/store'
 import { fetchProgram } from '../services/ProgramService';
+import Activity from './Activity'
 
 class ViewProgram extends Component {
 
@@ -13,22 +15,29 @@ class ViewProgram extends Component {
 
   render() {// strangely render here gets executed multiple times
     const program = store.getState().program
+    // const { alignItems, direction, justify } = this.state;
+
     return (
       <div>
         <h1>Program Title: {program && program.program.name}</h1>
         <p>Description: {program && program.program.description}</p>
         <Button type="button" variant='contained' color="primary" style={{ 'backgroundColor': 'orange' }}>Edit program</Button>
         <p>Unit: {program && program.program.unit.name}</p>
-        <p>Activities</p>
-        <ul>
-          {program && program.activities.map(activity => (
-            <li key={activity._id}>
-              <p>Activity Title: {activity.title}</p>
-              <p>Description: {activity.description}</p>
-              <a href={`/activities/${activity._id}`}><button>View</button></a>
-            </li>
-          ))}
-        </ul>
+        <h2>Activities</h2>
+
+        {/* Display Activity cards belonging to the program */}
+        <Grid
+          container
+          spacing={16}
+          // direction={direction}
+          // justify={justify}
+          // alignItems={alignItems}
+        >
+          {program && program.activities.map((activity) => (
+            <Activity key={activity._id} activity={activity} />
+            )
+          )}
+        </Grid>
 
       </div>
     )
